76bb335e2e42e068663173af356df16f0691ff41
Alazar Bekele <alazarthe70@gmail.com>
2024-11-21 16:35:23 +0300

    Not Migrations create problem

--- Base/models.py
+++ Base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+
+class Category (models.Model):
+
+    title = models.CharField (max_length=30)
+
+    def __str__(self):
+        return self.title
+    
+class index (models.Model):
+
+    Image = models.ImageField (upload_to='Photo/')
+    Title = models.CharField (max_length=30)
+    Price = models.IntegerField ()
+    Description = models.CharField (max_length=30)
+    Rating = models.IntegerField (validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.Title

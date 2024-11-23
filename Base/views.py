961a4934adb93d6c55d1a31ee57595930f41f2ea
AlazarBekele <alazarthe70@gmail.com>
2024-11-23 22:21:16 +0300

    Try to display

--- Base/views.py
+++ Base/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Index
+from .forms import LoginInput
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+
 
 # Create your views here.
 
@@ -15,6 +21,29 @@ def index (request):
 
     return render (request, 'index.html', context=context)
 
-def login_page (request):
 
-    return render (request, 'Login.html')
+
+def login_check (request):
+
+    form = LoginInput (request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is  None:
+                messages.success(request, 'No !!')
+            elif user is not None:
+                login (request, user)
+                messages.success (request, ('Yes!!'))
+                return redirect ('index')
+
+
+    context = {
+        'form' : form
+    }
+
+    return render (request, 'Login.html', context)

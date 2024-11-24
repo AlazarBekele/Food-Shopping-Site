5a5d3e67b0539f7857596e721167f27a3c4f368a
Alazar Bekele <alazarthe70@gmail.com>
2024-11-24 15:01:45 +0300

    Finsh login page & Background

--- Base/forms.py
+++ Base/forms.py
@@ -3,11 +3,11 @@ from django import forms
 class LoginInput (forms.Form):
 
   username = forms.CharField (widget=forms.TextInput(attrs={
-    'class' : 'form-control',
+    'class' : 'form-control rounded-pill',
     'placeholder' : 'Enter user name'
   }))
 
   password = forms.CharField (widget=forms.TextInput(attrs={
-    'class' : 'form-control',
+    'class' : 'form-control rounded-pill',
     'type' : 'passowrd'
   }))

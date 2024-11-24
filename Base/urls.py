71d4cff11edc2267c8601617470ade984f9d802d
Alazar Bekele <alazarthe70@gmail.com>
2024-11-24 17:54:18 +0300

    solve image bug

--- Base/urls.py
+++ Base/urls.py
@@ -3,9 +3,13 @@ from .views import (
     index,
     login_check
 ) 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path ('', index, name='index'),
     path ('login/', login_check, name='login_check')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
